0c18d45e5864b2236da0115b79f7e7e31d305b1e
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-08-17 23:27:32 +0530

    Update sign.jsx

--- src/component/nav/sign.jsx
+++ src/component/nav/sign.jsx
@@ -1,6 +1,6 @@
 import React, {  useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 // import { useAuth } from "../../context/auth";
 const Sign = () => {
   const[name,setName] = useState("")
@@ -9,7 +9,7 @@ const Sign = () => {
   const[cpassword,setCpassword] = useState("")
   // const[passwordMatch,setPasswordMatch] = useState(false)
 
-  
+  const navigate = useNavigate();
 
   const register = async(e) =>{
     e.preventDefault()
@@ -30,6 +30,7 @@ const Sign = () => {
         setEmail("")
         setPassword("")
         setCpassword("")
+      navigate("/login")
         
         
        
